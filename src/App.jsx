64735f870e97a3ab2a11838135911e5bec44bab8
Alicia Zuliani <luzul2424@gmail.com>
2023-01-09 22:39:10 -0300

    ItemDetail

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 import Navbar from "./components/Navbar";
 import Login from "./components/Login"
 import ItemListContainer from "./components/ItemListContainer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetailContainer from "./components/ItemDetailContainer"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Routes>
        <Route path="/" element={ <ItemListContainer /> } />
        <Route path="/Login" element={ <Login /> } />
-
+       <Route path="/ItemDetailContainer/:itemId" element={ <ItemDetailContainer />} />
+       <Route path="*" element={ <Navigate to={"/"}/> }/>
       </Routes>
 
     </BrowserRouter>
